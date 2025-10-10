b8fa6268f09be79574b8aef2449d710c557dbe12
Marco Richetta <marcorichetta@gmail.com>
2025-10-10 02:20:03 +0200

    hackql

--- app/database.py
+++ app/database.py
@@ -12,6 +12,11 @@ load_dotenv()
 
 # Create SQLAlchemy engine
 # https://supabase.com/docs/guides/troubleshooting/using-sqlalchemy-with-supabase-FUqebT#deploying-to-auto-scaling-servers
+
+# Handle deprecated postgres:// URL scheme
+# https://github.com/sqlalchemy/sqlalchemy/issues/6083#issuecomment-801478013
+if Config.POSTGRES_URL.startswith("postgres://"):
+    Config.POSTGRES_URL = Config.POSTGRES_URL.replace("postgres://", "postgresql://", 1)
 engine = create_engine(Config.POSTGRES_URL, client_encoding="utf8", poolclass=NullPool)
 
 # Create session factory
